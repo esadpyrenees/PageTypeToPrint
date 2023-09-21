e40d5c4d8b9ed8930121fe9a3a3afbf95f59fb78
Julien Bidoret <julien@accentgrave.net>
2023-09-21 18:57:37 +0200

    Hotfix ArrayObj.php with #[\ReturnTypeWillChange] (PHP deprecated warning)

--- _inc/Kaoken/MarkdownIt/Common/ArrayObj.php
+++ _inc/Kaoken/MarkdownIt/Common/ArrayObj.php
@@ -71,6 +71,7 @@ class ArrayObj implements ArrayAccess
      * @return mixed Can return all value types.
      * @since 5.0.0
      */
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->container[$offset];
@@ -88,6 +89,7 @@ class ArrayObj implements ArrayAccess
      * @return void
      * @since 5.0.0
      */
+    #[\ReturnTypeWillChange]
     public function offsetSet($offset, $value)
     {
         if (is_null($offset)) {
@@ -106,6 +108,7 @@ class ArrayObj implements ArrayAccess
      * @return void
      * @since 5.0.0
      */
+    #[\ReturnTypeWillChange]
     public function offsetUnset($offset)
     {
         unset($this->container[$offset]);
